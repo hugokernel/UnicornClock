5eb07b9f4bd5022f8b8e9a11970ac8f668a22e90
Charles R <hugo@digitalspirit.org>
2023-07-24 18:42:45 +0200

    feat(common): add clip class

--- unicornclock/common.py
+++ unicornclock/common.py
@@ -1,3 +1,21 @@
+
+
+class Clip:
+
+    def __init__(self, graphics, x, y, width, height):
+        self.graphics = graphics
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __enter__(self):
+        self.graphics.set_clip(self.x, self.y, self.width, self.height)
+
+    def __exit__(self, *args):
+        self.graphics.remove_clip()
+
+
 class Position:
     LEFT = '__left__'
     CENTER = '__center__'
