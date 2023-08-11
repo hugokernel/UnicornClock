046c26b818b5ad79062efdd512c3c0a9022ec9b7
Charles R <hugo@digitalspirit.org>
2023-08-11 10:45:43 +0200

    feat(utils): add from hsv function

--- unicornclock/utils.py
+++ unicornclock/utils.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 
@@ -22,3 +23,27 @@ def debounce(ms=250):
         return wrapper
 
     return decorator
+
+
+@micropython.native  # noqa: F821
+def from_hsv(h, s, v):
+    i = math.floor(h * 6.0)
+    f = h * 6.0 - i
+    v *= 255.0
+    p = v * (1.0 - s)
+    q = v * (1.0 - f * s)
+    t = v * (1.0 - (1.0 - f) * s)
+
+    i = int(i) % 6
+    if i == 0:
+        return int(v), int(t), int(p)
+    if i == 1:
+        return int(q), int(v), int(p)
+    if i == 2:
+        return int(p), int(v), int(t)
+    if i == 3:
+        return int(p), int(q), int(v)
+    if i == 4:
+        return int(t), int(p), int(v)
+    if i == 5:
+        return int(v), int(p), int(q)
